aa2624223b35f9a9a012f605ee47b7c9ab62e549
Becky Blue <becky.bluedodge@gmail.com>
2017-07-24 12:22:11 -0700

    Add files via upload

---
+++ OrderManagment.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="OrderManagment.aspx.cs" Inherits="PapaBobs.Web.OrderManagment" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <asp:GridView ID ="orderGridView" runat ="server" OnRowCommand="orderGridView_RowCommand">
+                <Columns>
+                    <asp:ButtonField Text="Complete" />
+                </Columns>
+            </asp:GridView>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ OrderManagment.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PapaBobs.Web
+{
+    public partial class OrderManagment : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            refreshGridView();
+
+        }
+
+        protected void orderGridView_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            int index = Convert.ToInt32(e.CommandArgument);
+            GridViewRow row = orderGridView.Rows[index];
+            var value = row.Cells[1].Text.ToString();
+
+            var orderId = Guid.Parse(value);
+            Domain.OrderManager.CompleteOrder(orderId);
+
+            refreshGridView();
+        }
+
+        private void refreshGridView()
+        {
+            var orders = Domain.OrderManager.GetOrders();
+            orderGridView.DataSource = orders;
+            orderGridView.DataBind();
+
+
+        }
+    }
+}
